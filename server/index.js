f6a3f98b032d199e78b858c71c9d1d389c5d04f8
Mitul Patel <mitulpatel2003@gmail.com>
2026-05-18 08:59:13 -0700

    Prepare for Vercel deployment

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Enable CORS so frontend can talk to backend
 app.use(cors());
